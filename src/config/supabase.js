08b35896562e4939c272b12c5551fb170412f65f
Shree Kavin <146123247+Shreekavinmr@users.noreply.github.com>
2025-12-18 19:51:21 +0530

    supabase config change

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -2,12 +2,21 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase URL or Anon Key in .env');
+if (!supabaseUrl || !supabaseServiceKey) {
+  throw new Error('Missing Supabase URL or SERVICE ROLE KEY in .env');
 }
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(
+  supabaseUrl,
+  supabaseServiceKey,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  }
+);
 
 module.exports = supabase;
